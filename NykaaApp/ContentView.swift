7d70471f8d3c672f79b295b5183156a2a6cacb01
vikramsingh <vc7662@gmail.com>
2026-03-26 11:59:41 +0530

    Initial Commit

---
+++ NykaaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NykaaApp
+//
+//  Created by Vikram Singh Depawat on 26/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NykaaApp/NykaaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NykaaAppApp.swift
+//  NykaaApp
+//
+//  Created by Vikram Singh Depawat on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct NykaaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
